505389c938cb2b63974568cdec8490c9a9a5ee0c
Benjamin <benschuetze@gmx.de>
2022-05-31 13:52:23 +0200

    started working on header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'benjaminschuetze';
-  images = ['Demo1.jpg', 'Demo2.jpg', 'Demo3.jpg'];
-  currentImage = 0;
-  showImage = true;
+export class AppComponent{
 
-  ngOnInit() {
-    this.updateImage();
-  }
-  
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  title = 'benjaminschuetze';
+  images = ['Demo1.jpg', 'Demo2.jpg', 'Demo3.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'Born to code',
+    'Graduated at Developer Akademie'
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+  
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
+}
